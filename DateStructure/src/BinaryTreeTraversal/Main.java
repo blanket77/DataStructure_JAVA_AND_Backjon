4f2f7910dfe0b62c6b27c1601ec88fe6a88f9606
blanket77 <quantummik@naver.com>
2021-07-26 14:02:08 +0900

    backjoon add And inorder, preorder, postorder

---
+++ DateStructure/src/BinaryTreeTraversal/Main.java
@@ -0,0 +1,68 @@
+package BinaryTreeTraversal;
+
+class Node{
+	Node left;
+	Node right;
+	int data;
+	Node next = null;
+	Node(){};
+	Node(int d){
+		this.data = d ;
+	}
+}
+
+class Tree{
+	Node root;
+	
+	public void setRoot(Node node) {
+		this.root = node;
+	}
+	
+	public Node getRoot() {
+		return root;
+	}
+	
+	public Node makeNode(int data, Node left, Node right) {
+		Node node = new Node();
+		node.data = data;
+		node.left = left;
+		node.right = right;
+		return node;
+	}
+	
+	//root Áß¾Ó
+	public void Inorder(Node node) {
+		if(node != null) {
+			Inorder(node.left);
+			System.out.println(node.data);
+			Inorder(node.right);
+		}
+	}
+	
+	//root ¾Õ
+	public void preorder(Node node) {
+		if(node != null) {
+			System.out.println(node.data);
+			preorder(node.left);
+			preorder(node.right);
+		}
+	}
+	
+	//root µÚ
+	public void postorder(Node node) {
+		if(node != null) {
+			postorder(node.left);
+			postorder(node.right);
+			System.out.println(node.data);
+		}
+	}
+}
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
